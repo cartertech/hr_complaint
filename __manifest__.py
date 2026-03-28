31afbb2f244def4aabe59b6295c553ffe69b791a
Nathan Carter <nathan@carter-tech.com.au>
2026-03-28 19:38:20 +1100

    changes to run under Odoo 17.0
    	modified:   .gitignore
    	modified:   LICENSE
    	modified:   README.md
    	modified:   __init__.py
    	modified:   __manifest__.py
    	modified:   models/__init__.py
    	modified:   models/hr_complaint.py
    	modified:   report/hr_complaint_report.xml
    	modified:   security/ir.model.access.csv
    	modified:   views/hr_complaint_view.xml

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Employee Complaints',
-    'version': '14.0.1.0.0',
+    'version': '1.0.0',
 	'license': 'AGPL-3',
     'author': 'One Click Software',
     'category': 'Human Resources',
